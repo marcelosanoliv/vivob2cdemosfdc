045634cef4b801ab067d634c43535f07a5cfc5a1
marcelosanoliv <marcelosanoliv@gmail.com>
2019-08-07 14:37:41 -0300

    vivo b2c demo lp

---
+++ colorlib-wizard-3/js/main.js
@@ -0,0 +1,46 @@
+$(function(){
+	$("#wizard").steps({
+        headerTag: "h4",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        titleTemplate :'<span class="number">#index#</span>',
+        labels: {
+            current: "",
+            finish: "Enviar",
+            next: "Próximo",
+            previous: "Anterior"
+        },
+        onStepChanging: function (event, currentIndex, newIndex) { 
+            if ( newIndex >= 1 ) {
+                $('.steps ul li:first-child a .step-arrow').remove();
+            } else {
+                $('.steps ul li:first-child a').append('<img src="images/step-arrow.png" alt="" class="step-arrow">');
+            }
+
+            if ( newIndex === 1 ) {
+                $('.steps ul li:nth-child(2) a').append('<img src="images/step-arrow.png" alt="" class="step-arrow">');;
+            } else {
+                $('.steps ul li:nth-child(2) a .step-arrow').remove();
+            }
+
+            if ( newIndex === 2 ) {
+                $('.steps ul li:nth-child(3) a').append('<img src="images/step-arrow.png" alt="" class="step-arrow">');;
+            } else {
+                $('.steps ul li:nth-child(3) a .step-arrow').remove();
+            }
+
+            if ( newIndex === 3 ) {
+                $('.steps ul li:nth-child(4) a').append('<img src="images/step-arrow.png" alt="" class="step-arrow">');;
+            } else {
+                $('.steps ul li:nth-child(4) a .step-arrow').remove();
+            }
+            return true; 
+        }
+    });
+    // Create Steps Image
+    $('.steps ul li:first-child a').append('<img src="images/step-1.png" alt="">').append('<img src="images/step-arrow.png" alt="" class="step-arrow">');
+    $('.steps ul li:nth-child(2) a').append('<img src="images/step-2.png" alt="">');
+    $('.steps ul li:last-child a').append('<img src="images/step-3.png" alt="">');
+})
+
